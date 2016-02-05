3ac37ee37b043dff018e154b692dcc9d085f58cd
shatunrus <shatunrus@gmail.com>
2016-02-05 15:07:29 +0300

    add coffie and hagover

---
+++ hacker_scripts/badun.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source ./config.cfg
+
+if [ w | grep -c "$mylogin" -eq 0 ]; then
+	sendEmail -f $mymail -t $wifemail -u "today a neet to work at home" -m $"I'm sorry, but im illing today. Work at home" -s smtp.example.ru
+fi
+
---
+++ hacker_scripts/coffie-blyat.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+coffe_ip="192.168.88.2"
+sleep 17 && ssh root@$coffe_ip 'some_magic_symbols_to_activate_coffemachine'
