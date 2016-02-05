f99315911a267f2504b7baa6068c1cda642bf254
shatunrus <shatunrus@gmail.com>
2016-02-05 14:54:32 +0300

    !

--- hacker_scripts/kumar-mudak.sh
+++ hacker_scripts/kumar-mudak.sh
@@ -8,7 +8,7 @@ newmail=$(ssh $LOGNAME@$eximhost 'cd /var/log/exim/$(date +%Y%m%d) && grep '$kum
 
 if [ ! -z $newmail ]; then
 	message_id=$(echo $newmail | awk '{ print $3 }')
-	message_tm=$(grep $kumar ./*.log | egrep "sorry" | grep -oE "T=\"[[:alpha:]]{1,50}\"" | sed -e "s/T=\"//" | sed "s/\"//")
+	message_tm=$(echo $newmail | grep -oE "T=\"[[:alpha:]]{1,50}\"" | sed -e "s/T=\"//" | sed "s/\"//")
 	if [ $(grep $message_id ./answered_kumar_mail) -eq 0 ]; then
 		echo "$message_id" >> ./answered_kumar_mail.txt
 		mysql -u <root_user> -p<root_pass> <kumar_db> < $( ll -tr /opt/backup/mysql/ | grep "kumar_db" | tail -n1)
