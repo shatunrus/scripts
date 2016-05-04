82625aba453365f4c0f805655ecd75f20a52a76b
Dmitry Shatov <shatun@li.ru>
2016-05-04 17:32:35 +0300

    Update crons_to_yaml.sh

--- crons_to_yaml.sh
+++ crons_to_yaml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -ne "crons:\n"
- crontab -l -u user | grep -v "^#" | sed "s/\*//g" | while read line; do
+crontab -l -u user | grep -v "^#" | sed "s/\*//g" | while read line; do
 echo -ne "  '$(echo $line | awk -F'/' '{ print $NF }' | awk '{ print $1}' | sed 's/\.php//g')':\n"
 if [ $(echo $line | awk '{ print $2 }' | grep -c ',') -gt 0 ]; then
 	echo -ne "     hour:\n"
