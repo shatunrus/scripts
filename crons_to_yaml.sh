41d44669e9b21efb552e0b4a1b006e728a0eacba
Dmitry Shatov <shatun@li.ru>
2016-05-04 17:22:35 +0300

    Added files via upload

---
+++ crons_to_yaml.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cat ./crons | sed "s/\*//g" | while read line; do
+echo -ne "  '$(echo $line | awk -F'/' '{ print $NF }' | awk '{ print $1}' | sed 's/\.php//g')':\n"
+if [ $(echo $line | awk '{ print $2 }' | grep -c ',') -gt 0 ]; then
+	echo -ne "     hour:\n"
+	echo -ne "$(echo $line | awk '{ print $2 }' | tr ',' '\n' | sed -e 's/^/       - /g')\n"
+else 
+	echo -ne "     hour: $(echo $line | awk '{ print $2 }')\n"
+fi
+echo -ne "     minute: $(echo $line | awk '{ print $1 }')\n"
+echo -ne "     command: \"$(echo $line | cut -d' ' -f 3- )\"\n "
+echo -ne "    user: 'check'\n"
+done
+
+
