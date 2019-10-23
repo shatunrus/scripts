326b1c0554439a676f8f852b087ca17450472c7d
shatov-dk <shatov-dk@paymgate.ru>
2019-10-23 16:49:22 +0300

    some fixes

--- logmove.sh
+++ logmove.sh
@@ -32,14 +32,13 @@ done
 if [ ! -z "$files" ]; then
   for i in  ${files[@]}; do
     eval $(echo $i | awk -F'-' '{ print "dstf="substr($NF,1,4)"/"substr($NF,5,2)"/"substr($NF,7,2)}')
-    appname=$(dirname "$i")
-    dstfold="$logpath${appname#$logpath}/$dstf"
+    dstfold="$(dirname "$i")/$dstf"
     shortname=$(basename $i | sed "s/.\{,9\}$//")
     [ ! -d $dstfold ] && mkdir -p $dstfold
     if [ -f $dstfold/$shortname ]; then
       if [ $(find $dstfold -name "${shortname}_*" | wc -l) -gt 0 ]; then
-        max_file=$(find $dstfold -name "${shortname}_*" | awk '{print length, $0}' | sort -nr | head -n1)
-        shortname=${shortname}_$[${max_file:(-2)}+1]
+        max_file=$(find $dstfold -name "${shortname}_*" | sort -nr | head -n1)
+        shortname=${shortname}_0$[${max_file:(-2)}+1]
       else
         shortname=${shortname}_01
       fi
@@ -49,3 +48,4 @@ if [ ! -z "$files" ]; then
   done
   echo "$(basename $0) started at $(date) and worked $SECONDS sec." >> $this_log
 fi
+
